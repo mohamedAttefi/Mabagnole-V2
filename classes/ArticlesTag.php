11d42b35f1392e51240b5557abbf7e51d707f7dd
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-07 15:38:39 +0100

    add articlestag class

---
+++ classes/ArticlesTag.php
@@ -0,0 +1,52 @@
+<?php
+include_once "DataBase.php";
+
+class ArticlesTag
+{
+    private $article_id;
+    private $tag_id;
+    private static ?PDO $pdo = null;
+
+    public function __construct($article_id, $tag_id)
+    {
+        $this->article_id = $article_id;
+        $this->tag_id = $tag_id;
+        self::initPDO();
+    }
+
+    private static function initPDO()
+    {
+        if (self::$pdo === null) {
+            self::$pdo = DataBase::getInstance()->getConnection();
+        }
+    }
+
+    public function add()
+    {
+        self::initPDO();
+        $stmt = self::$pdo->prepare("insert into article_tags (article_id, tag_id) value(?,?)");
+        $result = $stmt->execute([$this->article_id, $this->tag_id]);
+
+        if($result){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static function getTagForArticle($id)
+    {
+        self::initPDO();
+        $stmt = self::$pdo->prepare("SELECT *
+        FROM blog_tags t
+        JOIN article_tags at ON t.id = at.tag_id
+        JOIN blog_articles a ON a.id = at.article_id
+        WHERE a.id = ?;");
+        $result = $stmt->execute([$id]);
+        if ($result) {
+            return $stmt->fetchall(PDO::FETCH_ASSOC);
+        } else {
+            return null;
+        }
+    }
+}
