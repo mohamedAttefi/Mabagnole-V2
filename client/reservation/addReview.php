065de87d0cafae13c2fc87dfd9da97ab65c7c0ec
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-07 15:35:04 +0100

    feat: create logic to add a review on a reservation

---
+++ client/reservation/addReview.php
@@ -0,0 +1,13 @@
+<?php 
+include "../../classes/Review.php";
+session_start();
+$user_id = $_POST["user_id"];
+$reservation_id = $_POST["reservation_id"];
+$vehicule_id = $_POST["vehicule_id"];
+$note = $_POST["note"];
+$comment = $_POST["comment"];
+
+echo $user_id ." ". $reservation_id." ". $vehicule_id." ". $note." ". $comment;
+$Review = new Review(null, $user_id, $vehicule_id, $reservation_id, $note, $comment, null);
+$Review->create();
+
