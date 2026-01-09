5d5ee5ec084a910ccb0e7bde79878b330a60086b
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-09 19:05:28 +0100

    update vehicle class

--- classes/Vehicle.php
+++ classes/Vehicle.php
@@ -18,7 +18,7 @@ class Vehicle
 
     private static ?PDO $pdo = null;
 
-    public function __construct($id = null, $marque, $modele, $annee, $immatriculation, $categorie_id, $prix_journalier, $carburant, $nb_places, $description, $image_url, $disponible)
+    public function __construct($marque, $modele, $annee, $immatriculation, $categorie_id, $prix_journalier, $carburant, $nb_places, $description, $image_url, $disponible = null, $id = null)
     {
         $this->id = $id;
         $this->marque = $marque;
@@ -46,7 +46,10 @@ class Vehicle
     public static function all()
     {
         self::initPDO();
-        $stmt = self::$pdo->prepare("SELECT * from listevehicules where disponible = 1");
+        $sql = "SELECT * from listevehicules where 1=1 and disponible = 1";
+
+        $stmt = self::$pdo->prepare($sql);
+
         $stmt->execute();
         return $stmt->fetchall();
     }
@@ -54,18 +57,28 @@ class Vehicle
     public static function find(int $id)
     {
         self::initPDO();
-        $stmt = self::$pdo->prepare("SELECT * FROM listevehicules WHERE id = ?");
+        $stmt = self::$pdo->prepare("SELECT * FROM listevehicules WHERE id = ? and disponible = 1");
         $stmt->execute([$id]);
         return $stmt->fetch();
     }
 
-    public function ajouterVehicule($data)
+    public function ajouterVehicule()
     {
         self::initPDO();
         $stmt = self::$pdo->prepare("insert into vehicules (marque, modele, annee, immatriculation, categorie_id, prix_journalier, nb_places, description, image_url, carburant) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$data["marque"], $data["marque"], $data["modele"], $data["annee"], $data["immatriculation"], $data["categorie_id"], $data["prix_journalier"], $data["nb_places"], $data["description"], $data["image_url"], $data["carburant"]]);
+        echo $this->categorie_id;
+        $stmt->execute([$this->marque, $this->modele, $this->annee, $this->immatriculation, (int)$this->categorie_id, $this->prix_journalier, $this->nb_places, $this->description, $this->image_url, $this->carburant]);
         return $stmt->fetch();
     }
+
+    public function updateVehicule()
+    {
+        self::initPDO();
+        $stmt = self::$pdo->prepare("update vehicules set marque = ? , modele = ?, annee = ?, immatriculation = ?, categorie_id = ?, prix_journalier = ?, nb_places = ?, description = ?, image_url = ?, carburant = ? where id = ?");
+        $stmt->execute([$this->marque, $this->modele, $this->annee, $this->immatriculation, (int)$this->categorie_id, $this->prix_journalier, $this->nb_places, $this->description, $this->image_url, $this->carburant, $this->id]);
+        return $stmt->fetch();
+    }
+
 
     public function __get($att)
     {
@@ -75,5 +88,16 @@ class Vehicle
     public function __set($att, $value)
     {
         $this->$att = $value;
+    }
+
+    public static function deleteVehicle($id)
+    {
+        self::initPDO();
+        $stmt = self::$pdo->prepare("update vehicules set disponible = false where id = ?");
+        $result = $stmt->execute([$id]);
+        if ($result) {
+            return true;
+        }
+        return null;
     }
 }
