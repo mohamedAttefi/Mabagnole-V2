c09375a63a917adfdf4c656955162d02085c9952
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-09 18:58:45 +0100

    create add comment on an article

--- public/blogs/add_comment.php
+++ public/blogs/add_comment.php
@@ -1,13 +1,19 @@
 <?php
 include "../../classes/BlogComment.php";
-    session_start();
-    $user_id = $_SESSION["user_id"];
-    $content = $_POST["content"];
-    $id = $_POST["article_id"];
-    $data = ["article_id" => $id, "user_id" => $user_id, "content" => $content];
-    $resultat = BlogComment::addComment($data);
-    if ($resultat) {
-        header("location: blog-article.php");
-        exit;
-    }
+session_start();
+if(!isset($_SESSION["user_id"])){
+    header("location: ../login.php");
+    exit;
+}
 
+
+
+$user_id = $_SESSION["user_id"];
+$content = $_POST["content"];
+$id = $_POST["article_id"];
+$data = ["article_id" => $id, "user_id" => $user_id, "content" => $content];
+$resultat = BlogComment::addComment($data);
+if ($resultat) {
+    header("location: blog-article.php");
+    exit;
+}
