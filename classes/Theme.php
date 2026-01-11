37dceb69df21b677679973a0407215593fc636cb
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-11 18:32:05 +0100

    fix theme class

--- classes/Theme.php
+++ classes/Theme.php
@@ -28,8 +28,26 @@ class Theme
     public static function all()
     {
         self::initPDO();
-        $stmt = self::$pdo->prepare("SELECT * from themes");
+        $stmt = self::$pdo->prepare("SELECT t.id, t.name,t.description,b.created_at, count(b.id) as total from themes t left join blog_articles b on t.id = b.theme_id GROUP BY b.theme_id, t.name
+");
         $stmt->execute();
         return $stmt->fetchall();
+    }
+
+    public static function delete($id){
+        self::initPDO();
+        $stmt = self::$pdo->prepare("delete themes where id = ?");
+        $stmt->execute([$id]);
+    }
+    public function create(){
+        self::initPDO();
+        $stmt = self::$pdo->prepare("insert into themes (name, description) values (?,?)");
+        $stmt->execute([$this->name, $this->description]);
+    }
+
+    public function update(){
+        self::initPDO();
+        $stmt = self::$pdo->prepare("update themes set name = ?, description = ? where id = ?");
+        $stmt->execute([$this->name, $this->description, $this->id]);
     }
 }
