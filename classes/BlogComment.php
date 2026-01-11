fe267f028e4ed47c3b1ea1a02b8568c9f52cd2c8
mohamedAttefi <attefimohamed0@gmail.com>
2026-01-11 18:29:51 +0100

    fix comment class

--- classes/BlogComment.php
+++ classes/BlogComment.php
@@ -40,6 +40,19 @@ class BlogComment
             return null;
         }
     }
+    public static function findById($id)
+    {
+        self::initPDO();
+        $stmt = self::$pdo->prepare("SELECT c.*,a.content,a.title, a.theme_id, a.status, a.created_at,  c.content as commentaire, u.nom as user_name from blog_comments c join utilisateurs u on c.user_id = u.id join blog_articles a on c.article_id = a.id where c.id = ?");
+        $result = $stmt->execute([$id]);
+        if($result){
+            $row = $stmt->fetch();
+            return new BlogComment($row["id"], $row["article_id"], $row["user_id"], $row["content"], $row["created_at"]);
+        }
+        else{
+            return null;
+        }
+    }
 
     public static function addComment($data)
     {
@@ -51,5 +64,17 @@ class BlogComment
         }else{
             return null;
         }
+    }
+    public static function all(){
+        self::initPDO();
+        $stmt = self::$pdo->prepare("select b.*, u.nom from blog_comments b join utilisateurs u on b.user_id = u.id");
+        $stmt->execute();
+        return $stmt->fetchall();
+    }
+
+    public function delete(){
+        self::initPDO();
+        $stmt = self::$pdo->prepare("delete from blog_comments where id = ?");
+        $stmt->execute([$this->id]);
     }
 }
